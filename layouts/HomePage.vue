c92b3cda9f0cf03b3732c729109121b16c386882
zh-lx <18366276315@163.com>
2021-05-03 17:06:22 +0800

    feat: modifying the reference order of loading components

--- layouts/HomePage.vue
+++ layouts/HomePage.vue
@@ -9,17 +9,17 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, reactive, toRefs } from 'vue';
+import Loading from '@/components/Loading.vue';
 import Home from '@/components/Home.vue';
 import LayoutContainer from '@/components/LayoutContainer.vue';
-import Loading from '@/components/Loading.vue';
 
 export default defineComponent({
   name: 'HomePage',
 
   components: {
+    Loading,
     Home,
     LayoutContainer,
-    Loading,
   },
 
   setup(props) {
@@ -36,7 +36,6 @@ export default defineComponent({
       const src = window
         .getComputedStyle(document.querySelector('#_homeBgContainer'))
         .background.match(/url\(\"?(.*)\"\)/)[1];
-      console.log(src);
       const img = new Image();
       img.src = src;
       img.onload = function () {
