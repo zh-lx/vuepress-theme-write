07361ba2efc566359d0330a15060c58a499f3524
zh-lx <18366276315@163.com>
2021-05-13 00:09:20 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'no-unused-vars': ['warn'],
+  },
+};
