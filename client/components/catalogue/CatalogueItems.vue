f25b2e8e64a8b3a41248e9c83e67f6411bf5f174
zh-lx <18366276315@163.com>
2022-03-01 23:47:12 +0800

    feat: 优化目录自动滚动

--- client/components/catalogue/CatalogueItems.vue
+++ client/components/catalogue/CatalogueItems.vue
@@ -1,47 +1,35 @@
 <script setup lang="ts">
 import { useCatalogues } from '@/composables';
 import { watch, nextTick } from 'vue';
-import { useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
 import CatalogueItem from './CatalogueItem.vue';
 
 const catalogueItems = useCatalogues();
-const route = useRouter();
+const route = useRoute();
 // 目录超出页面时，自动跟随滚动
 watch(
-  () => route,
-  (val) => {
-    watch(
-      () => route,
-      (val) => {
-        console.log(val.currentRoute.value);
-        const activeSidebarItem = document.querySelector(
-          `[href="${val.currentRoute.value.path}${val.currentRoute.value.hash}"]`
-        );
-        const sidebar = document.querySelector('#catalogues-aside');
-        if (activeSidebarItem && sidebar) {
-          const activeSidebarItemTop =
-            activeSidebarItem.getBoundingClientRect().top;
-          const activeSidebarItemHeight =
-            activeSidebarItem.getBoundingClientRect().height;
-          const sidebarTop = sidebar.getBoundingClientRect().top;
-          const sidebarHeight = sidebar.getBoundingClientRect().height;
-          if (activeSidebarItemTop < sidebarTop) {
-            activeSidebarItem.scrollIntoView(true);
-          } else if (
-            activeSidebarItemTop + activeSidebarItemHeight >
-            sidebarTop + sidebarHeight
-          ) {
-            activeSidebarItem.scrollIntoView(false);
-          }
-        }
-      },
-      {
-        deep: true,
-      }
+  () => route.hash,
+  (hash) => {
+    const activeSidebarItem = document.querySelector(
+      `[href="${route.path}${hash}"]`
     );
-  },
-  {
-    deep: true,
+    const sidebar = document.querySelector('#catalogues-aside');
+    if (activeSidebarItem && sidebar) {
+      const activeSidebarItemTop =
+        activeSidebarItem.getBoundingClientRect().top;
+      const activeSidebarItemHeight =
+        activeSidebarItem.getBoundingClientRect().height;
+      const sidebarTop = sidebar.getBoundingClientRect().top;
+      const sidebarHeight = sidebar.getBoundingClientRect().height;
+      if (activeSidebarItemTop < sidebarTop) {
+        activeSidebarItem.scrollIntoView(true);
+      } else if (
+        activeSidebarItemTop + activeSidebarItemHeight >
+        sidebarTop + sidebarHeight
+      ) {
+        activeSidebarItem.scrollIntoView(false);
+      }
+    }
   }
 );
 </script>
